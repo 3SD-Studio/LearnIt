dba762ee46cb3743a2b7412e9ec7530dcd100b41
Dave <dj300758@student.polsl.pl>
2023-09-24 22:20:31 +0200

    Navbar created.

---
+++ frontend/src/components/navbar/navbar-button/navbar-button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './navbar-button.css'
+
+const Button = () => 
+    <div id="button">
+        <a href='https://calamitymod.wiki.gg/wiki/Guide:Class_setups'>placeholder</a>
+    </div>
+
+export default Button
