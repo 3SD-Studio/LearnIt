c7cb65a07063b05e980acb7e88e3c9fb1839e178
Michał Pilch <m.pilch2002@gmail.com>
2023-09-28 11:40:12 +0200

    Created documentation examples and fixed indent

--- frontend/src/components/navbar/navbar-button/navbar-button.tsx
+++ frontend/src/components/navbar/navbar-button/navbar-button.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import './navbar-button.css'
 
-const Button = (props:any) => {
-    console.log(props);
-    return <div>
-                <a href={props.link}>
-                    <button className="navbar-button">
-                        <span>{props.buttonName}</span>
-                    </button>
-                </a>
-            </div>
+
+/**
+ * Button component for navbar 
+ * @author Dawid Jeziorski <@Dave848>
+ * @param props - must contain link and name 
+ * @returns JSX component responsible for navbar button
+ */
+const Button = (props: any) => {
+  console.log(props);
+  return <div>
+    <a href={props.link}>
+      <button className="navbar-button">
+        <span>{props.buttonName}</span>
+      </button>
+    </a>
+  </div>
 }
 
 export default Button
--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Button from './navbar-button/navbar-button'
 import './navbar.css';
 
-
-const Navbar = () => 
+/**
+ * Navbar component 
+ * @author Dawid Jeziorski <@Dave848>
+ * @returns {JSX} JSX component responsible for navbar
+ */
+const Navbar = () =>
+  <>
     <div id="main-contener">
-        <h1 id="site-name">LearnIt</h1>
-        <Button link="https://www.google.com/" buttonName="button-name"/>
-        <Button link="https://www.google.com/" buttonName="button-name"/>
-        <Button link="https://www.google.com/" buttonName="button-name"/>
-        <Button link="https://www.google.com/" buttonName="button-name"/>
+      <h1 id="site-name">LearnIt</h1>
+      <Button link="https://www.google.com/" buttonName="button-name" />
+      <Button link="https://www.google.com/" buttonName="button-name" />
+      <Button link="https://www.google.com/" buttonName="button-name" />
+      <Button link="https://www.google.com/" buttonName="button-name" />
     </div>
+  </>
+
 
 export default Navbar;
